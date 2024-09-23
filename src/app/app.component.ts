2334759264bc882e80718d86f66bf2d546a7c2b2
Amit Kapur <amit.kapur@autogeneral.com.au>
2024-09-23 14:23:11 +1000

    fixed the build issues by commenting out code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,19 +27,19 @@ export class AppComponent implements OnInit {
   authService = inject(AuthService);
 
   ngOnInit(): void {
-    this.authService.user$.subscribe(
-      (user: { email: string; displayName: string }) => {
-        if (user) {
-          this.authService.currentUserSig.set({
-            email: user.email!,
-            username: user.displayName!,
-          });
-        } else {
-          this.authService.currentUserSig.set(null);
-        }
-        console.log(this.authService.currentUserSig());
-      }
-    );
+    // this.authService.user$.subscribe(
+    //   (user: { email: string; displayName: string }) => {
+    //     if (user) {
+    //       this.authService.currentUserSig.set({
+    //         email: user.email!,
+    //         username: user.displayName!,
+    //       });
+    //     } else {
+    //       this.authService.currentUserSig.set(null);
+    //     }
+    //     console.log(this.authService.currentUserSig());
+    //   }
+    // );
   }
 
   logout(): void {
